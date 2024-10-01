44a39d37988f29a31efc48fd5a8e5b86bc1f540a
Raphael Pitol <raphaelpitoljuliani@gmail.com>
2024-10-01 20:39:19 -0300

    Criando repositorio

---
+++ MeusLivros.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeusLivros.Domain.Entities
+{
+    internal class Entity : IEquatable<Entity>
+    {
+        public int Id { get; set; }
+
+        public bool Equals(Entity? other)
+        {
+            return other?.Id == Id;
+          
+        }
+    }
+}
